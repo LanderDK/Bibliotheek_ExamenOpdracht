bc29d415ff3ab81506a4d2828453fada17555533
Skippn <landerdekesel@gmail.com>
2023-05-23 11:56:03 +0200

    Finished 2.0

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
+INSERT INTO users(username,password,enabled, max_aantal_favs)
+VALUES('Baba', '$2a$10$dhIbfqE5r5p0G2v2.mJlh.93rpCsJRHhDlbZj78ixfdojKWdhhYJq', true, 7),('Lander', '$2a$10$PVJgEDbszumX2z1glP6PI.jWd5LuLcijjS5rZggicBoL/gqNHPg8C', true, 5);
+
 INSERT INTO authorities(username,authority) 
 VALUES ('Baba', 'ROLE_USER'),('Lander', 'ROLE_ADMIN');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,13 @@
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS authorities;
+
+CREATE table users (
+  username varchar(255) NOT NULL,
+  enabled bit(1) DEFAULT NULL,
+  favorieten varbinary(255) DEFAULT NULL,
+  max_aantal_favs int DEFAULT NULL,
+  password varchar(255) DEFAULT NULL,
+  PRIMARY KEY (username));
 
 create table authorities (
   username varchar(50) not null,
